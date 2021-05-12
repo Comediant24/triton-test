b756fb4194504cc3004d9e27df39b1923abe05a9
Iurii Bairamukov <juribai@mail.ru>
2021-05-12 13:55:22 +0700

    feat: добавить в проект пакет react-router-dom
    Установить reac-router-dom
    
    Установить пакет типов для него

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { Layout } from './layouts';
 import { Main } from './pages/main';
 
 function App() {
   return (
-    <Layout>
-      <Main />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route exact path="/basket"></Route>
+          <Route exact path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
