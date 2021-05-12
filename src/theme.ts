0165b90dafdcec30af36e358c9adf5dace2a4276
Iurii Bairamukov <juribai@mail.ru>
2021-05-12 09:29:53 +0700

    feat: организовать структуру, добавить styled
    Удалить дефолтные файлы CRA
    
    Добавить пакет Styled Components и его типы для TS

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'black',
+    secondary: 'magenta',
+  },
+};
+
+export { myTheme };
